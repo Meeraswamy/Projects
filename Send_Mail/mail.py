9bd1cf40ae7a38acb4bc45ad035da3795b06961f
Meeraswamy <meeraswamy555@gmail.com>
2022-07-19 22:27:47 +0530

    Second commit

---
+++ Send_Mail/mail.py
@@ -0,0 +1,32 @@
+# Python code to Sending mail from your Gmail account
+import smtplib
+
+# creates SMTP session
+#smtp.gmail.com is the Gmail server to send gmails using
+#third party email clients
+session = smtplib.SMTP('smtp.gmail.com', 587)
+
+# start TLS for security
+# It Puts the SMTP connection in TLS mode
+# All SMTP commands that follow will be encrypted
+session.starttls()
+
+# Authentication
+sender_email_id = input("Enter sender email ID : ")
+sender_email_id_password = input("Enter sender email id password : ")
+receiver_email_id = input("Enter receiver email ID : ")
+# login to SMTP server using Gmail credentials
+session.login(sender_email_id,sender_email_id_password)
+
+# message to be sent
+message = input("Message_you_need_to_send : ")
+
+# sending the mail
+# sends mail to receiver email ID
+session.sendmail(sender_email_id,receiver_email_id,message)
+
+# terminating the session
+# we should close our session.
+session.quit()
+
+print("Email Sent..... ")
